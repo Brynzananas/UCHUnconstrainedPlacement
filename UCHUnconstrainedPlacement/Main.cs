09c2dc19d70bc4634e82b0f244d421768bb4ed32
Brynzananas <brynzananas@gmail.com>
2025-07-03 21:48:36 +0600

    Makes all blocks available for rotation

--- UCHUnconstrainedPlacement/Main.cs
+++ UCHUnconstrainedPlacement/Main.cs
@@ -2,11 +2,11 @@
 using HarmonyLib;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using System;
 using System.Net;
 using System.Security;
 using System.Security.Permissions;
 using UnityEngine;
-
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 [module: UnverifiableCode]
 #pragma warning disable CS0618
@@ -19,16 +19,14 @@ namespace UncontrainedPlacement
     {
         public const string ModGuid = "com.brynzananas.uncontrainedplacement";
         public const string ModName = "Uncontrained Placement";
-        public const string ModVer = "1.0.0";
+        public const string ModVer = "1.1.0";
+        private Harmony harmonyPatcher;
         public void Awake()
         {
-            IL.PiecePlacementCursor.PerformLeftRotation += PiecePlacementCursor_PerformLeftRotation;
-            IL.PiecePlacementCursor.PerformRightRotation += PiecePlacementCursor_PerformRightRotation;
-            IL.PiecePlacementCursor.SetPiece += PiecePlacementCursor_SetPiece;
-            IL.PiecePlacementCursor.FixedUpdate += PiecePlacementCursor_FixedUpdate;
-            IL.Placeable.Place_int_bool_bool += Placeable_Place_int_bool_bool;
+            harmonyPatcher = new Harmony(ModGuid);
+            harmonyPatcher.CreateClassProcessor(typeof(Patches)).Patch();
         }
-        private static void RemovePlacementRounding(ILCursor iLCursor)
+        public static void RemovePlacementRounding(ILCursor iLCursor)
         {
             while (iLCursor.TryGotoNext(MoveType.Before,
                 x => x.MatchCall<Mathf>(nameof(Mathf.Round))
@@ -37,7 +35,7 @@ namespace UncontrainedPlacement
                 iLCursor.Remove();
             }
         }
-        private static void RemoveRotationRounding(ILCursor iLCursor, ILContext iLContext)
+        public static void RemoveRotationRounding(ILCursor iLCursor)
         {
             if (iLCursor.TryGotoNext(MoveType.Before,
                 x => x.MatchLdloc(0),
@@ -53,30 +51,81 @@ namespace UncontrainedPlacement
             }
             else
             {
-                Debug.LogError(iLContext.Method.Name + " IL hook failed");
+                Debug.LogError(iLCursor.Context.Method.Name + " IL hook failed");
             }
         }
-        private void PiecePlacementCursor_FixedUpdate(ILContext il)
+        public static void SetRotationOverride(ILCursor iLCursor)
         {
-            ILCursor c = new ILCursor(il);
-            RemovePlacementRounding(c);
+            ILLabel iLLabel = null;
+            if (iLCursor.TryGotoNext(MoveType.Before,
+                x => x.MatchLdarg(1),
+                x => x.MatchBrfalse(out _),
+                x => x.MatchLdarg(0),
+                x => x.MatchCall<PiecePlacementCursor>("get_Piece"),
+                x => x.MatchLdfld<Placeable>(nameof(Placeable.OrientationAlt)),
+                x => x.MatchLdcI4(out _),
+                x => x.MatchBneUn(out iLLabel),
+                x => x.MatchLdarg(0),
+                x => x.MatchCall<PiecePlacementCursor>("get_Piece"),
+                x => x.MatchLdfld<Placeable>(nameof(Placeable.Orientation)),
+                x => x.MatchBr(out _),
+                x => x.MatchLdarg(0),
+                x => x.MatchCall<PiecePlacementCursor>("get_Piece"),
+                x => x.MatchLdfld<Placeable>(nameof(Placeable.OrientationAlt)),
+                x => x.MatchStloc(1),
+                x => x.MatchLdloc(1),
+                x => x.MatchSwitch(out _)
+                ))
+            {
+                iLCursor.Index += 1;
+                iLCursor.RemoveRange(6);
+                iLCursor.Emit(OpCodes.Brtrue_S, iLLabel);
+                iLCursor.Index += 3;
+                iLCursor.Emit(OpCodes.Pop);
+                iLCursor.Emit(OpCodes.Ldc_I4, (int)Placeable.OrientMode.ROTATE);
+                iLCursor.Index += 4;
+                iLCursor.Emit(OpCodes.Pop);
+                iLCursor.Emit(OpCodes.Ldc_I4, (int)Placeable.OrientMode.FLIPX);
+            }
+            else
+            {
+                Debug.LogError(iLCursor.Context.Method.Name + " IL hook failed");
+            }
         }
-
-        private void Placeable_Place_int_bool_bool(ILContext il)
+    }
+    [HarmonyPatch]
+    class Patches
+    {
+        [HarmonyPatch(typeof(PiecePlacementCursor), nameof(PiecePlacementCursor.FixedUpdate))]
+        [HarmonyILManipulator]
+        private static void PiecePlacementCursor_FixedUpdate(ILContext il)
         {
             ILCursor c = new ILCursor(il);
-            RemoveRotationRounding(c, il);
+            Main.RemovePlacementRounding(c);
         }
-        private void PiecePlacementCursor_SetPiece(ILContext il)
+        [HarmonyPatch(typeof(Placeable), nameof(Placeable.Place), new Type[] { typeof(int), typeof(bool), typeof(bool) })]
+        [HarmonyILManipulator]
+        private static void Placeable_Place_int_bool_bool(ILContext il)
         {
             ILCursor c = new ILCursor(il);
-            RemoveRotationRounding(c, il);
+            Main.RemoveRotationRounding(c);
+        }
+        [HarmonyPatch(typeof(PiecePlacementCursor), nameof(PiecePlacementCursor.SetPiece))]
+        [HarmonyILManipulator]
+        private static void PiecePlacementCursor_SetPiece(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            Main.RemoveRotationRounding(c);
             c = new ILCursor(il);
-            RemovePlacementRounding(c);
+            Main.RemovePlacementRounding(c);
         }
-        private void PiecePlacementCursor_PerformRightRotation(ILContext il)
+        [HarmonyPatch(typeof(PiecePlacementCursor), nameof(PiecePlacementCursor.PerformRightRotation))]
+        [HarmonyILManipulator]
+        private static void PiecePlacementCursor_PerformRightRotation(ILContext il)
         {
             ILCursor c = new ILCursor(il);
+            Main.SetRotationOverride(c);
+            c = new ILCursor(il);
             if (c.TryGotoNext(MoveType.Before,
                 x => x.MatchLdcR4(-90f)
                 ))
@@ -89,9 +138,13 @@ namespace UncontrainedPlacement
                 Debug.LogError(il.Method.Name + " IL hook failed");
             }
         }
-        private void PiecePlacementCursor_PerformLeftRotation(ILContext il)
+        [HarmonyPatch(typeof(PiecePlacementCursor), nameof(PiecePlacementCursor.PerformLeftRotation))]
+        [HarmonyILManipulator]
+        private static void PiecePlacementCursor_PerformLeftRotation(ILContext il)
         {
             ILCursor c = new ILCursor(il);
+            Main.SetRotationOverride(c);
+            c = new ILCursor(il);
             if (c.TryGotoNext(MoveType.Before,
                 x => x.MatchLdcR4(90f)
                 ))
@@ -102,6 +155,26 @@ namespace UncontrainedPlacement
             else
             {
                 Debug.LogError(il.Method.Name + " IL hook failed");
+            }
+        }
+        [HarmonyPatch(typeof(SwingingAxe), nameof(SwingingAxe.Update))]
+        [HarmonyILManipulator]
+        private static void SwingingAxe_Update(ILContext il)
+        {
+            ILCursor iLCursor = new ILCursor(il);
+            if (iLCursor.TryGotoNext(MoveType.Before,
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld<SwingingAxe>(nameof(SwingingAxe.RotateLocked)),
+                x => x.MatchCallvirt<Component>("get_transform"),
+                x => x.MatchCall<Quaternion>("get_identity"),
+                x => x.MatchCallvirt<Transform>("set_rotation")
+                ))
+            {
+                iLCursor.RemoveRange(5);
+            }
+            else
+            {
+                Debug.LogError(iLCursor.Context.Method.Name + " IL hook failed");
             }
         }
     }
